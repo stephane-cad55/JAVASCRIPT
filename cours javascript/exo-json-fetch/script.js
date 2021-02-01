f377f2858f6aa77146064f2191300bae5dc6ad2c
ste <s.cadeck@gmail.com>
2021-02-01 17:31:05 +0100

    update exo-json-fetch, add visualisation

--- cours javascript/exo-json-fetch/script.js
+++ cours javascript/exo-json-fetch/script.js
@@ -1,24 +1,57 @@
-fetch('https://jsonplaceholder.typicode.com/users/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+var nav = document.querySelector('nav');
 
-fetch('https://jsonplaceholder.typicode.com/posts/2')
-    .then(response => response.json())
-    .then(json => console.log(json))
+function listUsers(jsonObj) {
+    for (var i = 0; i < jsonObj.length; i++) {
+        var nom = jsonObj[i].name;
+        console.log(nom);
+        var user = document.createElement('li');
+        user.textContent = nom;
+        nav.appendChild(user);
+    }
+
+}
+var p = fetch('https://jsonplaceholder.typicode.com/users')
+
+    .then(function (response) {
+        response.json()
+            .then(function (contenu) {
+                listUsers(contenu);
+                leHeader(contenu);
+
+            })
+    });
 
 var header = document.querySelector('header');
 
-var nav = document.querySelector('nav');
-
-var main = document.querySelector('main');
-
-//fonction pour afficher les utilisateurs
-
-//fonction pour afficher les posts des utilisateurs
-
-// fonction pour inserer le header
-function leHeader(json) {
-    var header = document.createElement('header');
-    header.textContent = 'users ' + json['users'];
-    header.appendChild(header);
+function leHeader(jsonObj) {
+    for (var i = 0; i < jsonObj.length; i++) {
+        var nom1 = jsonObj[i].name;
+        var toto = document.createElement('p');
+        toto.textContent = nom1;
+        header.appendChild(toto);
+    }
 }
+
+var yo = document.querySelector('main');
+
+function listPosts(jsonObj){
+    for (var i = 0; i < jsonObj.length; i++) {
+        var nom2 = jsonObj[i].name;
+       
+        var post = document.createElement('li');
+        post.textContent = nom2;
+        main.appendChild(post);
+    
+
+} 
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(function (response2) {
+    response2.json()
+        .then(function (contenu2) {
+           
+            listPosts(contenu2);
+
+        })
+});
